ff5d819ae31e5e7b8c16debcb7dd689e598ad44a
test <test@test>
2020-07-21 22:29:05 +0900

    update

--- Install_rcfiles.ps1
+++ Install_rcfiles.ps1
@@ -5,7 +5,7 @@ if (-not(Test-Path $profile))
 {
     New-Item -path $profile -type file -force
 }
-cp Microsoft.PowerShell_profile.ps1 $home\Documents\PowerShell\
+cp .\Microsoft.PowerShell_profile.ps1 $home\Documents\PowerShell
 
 
 # vim
--- Install_scoopApps.ps1
+++ Install_scoopApps.ps1
@@ -1,23 +1,26 @@
 cd $PSScriptRoot
 
+
+# git is first
+scoop install git
+
 # add buckets
 scoop bucket add extras
 scoop bucket add rkbk60 https://github.com/rkbk60/scoop-for-jp
 scoop bucket add jfut https://github.com/jfut/scoop-jfut.git
 scoop bucket add iyokan-jp https://github.com/tetradice/scoop-iyokan-jp
 
-# install apps
-scoop install android-studio
-scoop install git
+# other apps
+scoop install 7zip
 scoop install peco
 scoop install pwsh
 scoop install python
 scoop install busybox
 scoop install vim
 scoop install vscode
-scoop install freemind
-scoop install PhraseExpress
 scoop install sakura-editor
 scoop install winmerge-jp
 scoop install wireshark
 scoop install nodejs-lts
+scoop install android-studio
+scoop install sublime-text
